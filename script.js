351d6f1946b67b69f4029f39a2fed5b674aa88fd
Aditya Iyer <HungryAdi@users.noreply.github.com>
2016-06-20 12:13:58 -0700

    Uploading Files for site with overlay menu.
    Overlay Menu (slides down).

---
+++ script.js
@@ -0,0 +1,10 @@
+/* From http://www.w3schools.com/howto/howto_js_fullscreen_overlay.asp */
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
